d140e826fe66bde0c3410fee2c32523b2f120677
Alexei Labrada <labrada.alexei@gmail.com>
2022-02-22 21:14:55 -0500

    Reimplement biquad chain.

--- Neuronic.Filters/BiquadChain.cs
+++ Neuronic.Filters/BiquadChain.cs
@@ -8,7 +8,7 @@ namespace Neuronic.Filters
     /// <summary>
     /// Uses a sequence of biquad filters to implement a higher-order filter.
     /// </summary>
-    public class BiquadChain : IZeroPhaseDigitalFilter
+    public abstract class BiquadChain : IZeroPhaseDigitalFilter
 #if NET40
         , IEnumerable<Biquad>
 #else
@@ -16,43 +16,22 @@ namespace Neuronic.Filters
 #endif
     {
         private readonly Biquad[] _coeffs;
-        private readonly double[] _yn;
-        private readonly double[] _yn1;
-        private readonly double[] _yn2;
-        private double _xn1;
-        private double _xn2;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="BiquadChain"/> class.
         /// </summary>
         /// <param name="coefficients">The list of biquad sections.</param>
         /// <param name="gain">The overall gain of the filter.</param>
-        public BiquadChain(IList<Biquad> coefficients, double gain)
+        protected BiquadChain(IList<Biquad> coefficients)
         {
             _coeffs = new Biquad[coefficients.Count];
             coefficients.CopyTo(_coeffs, 0);
-            Gain = gain;
-            _yn = new double[_coeffs.Length];
-            _yn1 = new double[_coeffs.Length];
-            _yn2 = new double[_coeffs.Length];
         }
-
-        /// <summary>
-        /// Gets the overall gain of the filter.
-        /// </summary>
-        public double Gain { get; }
 
         /// <summary>
         /// Reset's the filter's state. Use this if the next buffer that will be processed is not continuous after the last one.
         /// </summary>
-        public void Reset()
-        {
-            _xn1 = 0;
-            _xn2 = 0;
-            Array.Clear(_yn, 0, Count);
-            Array.Clear(_yn1, 0, Count);
-            Array.Clear(_yn2, 0, Count);
-        }
+        public abstract void Reset();
 
         /// <summary>
         /// Executes one sweep of the filter.
@@ -61,41 +40,7 @@ namespace Neuronic.Filters
         /// <param name="output">The destination buffer. (Use the same value of <paramref name="input"/> to execute in place).</param>
         /// <param name="count">The number of samples to process.</param>
         /// <param name="stride">The length of the processing step. Can be used to bypass samples.</param>
-        public unsafe void FilterOnce(float* input, float* output, int count, int stride)
-        {
-            fixed (Biquad* coeffs = _coeffs)
-            {
-                fixed (double* yn = _yn, yn1 = _yn1, yn2 = _yn2)
-                {
-                    for (int n = 0; n < count; n++)
-                    {
-                        var xn = *input * Gain;
-
-                        yn[0] = coeffs[0].B0 * xn + coeffs[0].B1 * _xn1 + coeffs[0].B2 * _xn2
-                                + coeffs[0].A1 * yn1[0] + coeffs[0].A2 * yn2[0];
-
-                        for (int i = 1; i < Count; i++)
-                            yn[i] = coeffs[i].B0 * yn[i - 1] + coeffs[i].B1 * yn1[i - 1] + coeffs[i].B2 * yn2[i - 1]
-                                    + coeffs[i].A1 * yn1[i] + coeffs[i].A2 * yn2[i];
-
-                        // Shift delay line elements.
-                        for (int i = 0; i < Count; i++)
-                        {
-                            yn2[i] = yn1[i];
-                            yn1[i] = yn[i];
-                        }
-                        _xn2 = _xn1;
-                        _xn1 = xn;
-
-                        // Store result and stride
-                        *output = (float)yn[Count - 1];
-
-                        input += stride;
-                        output += stride;
-                    }
-                }
-            }
-        }
+        public abstract unsafe void FilterOnce(float* input, float* output, int count, int stride);
 
         /// <summary>
         /// Executes two sweeps of the filter (forward and backward). This is a zero-phase filter.
@@ -162,41 +107,7 @@ namespace Neuronic.Filters
         /// <param name="output">The destination buffer. (Use the same value of <paramref name="input"/> to execute in place).</param>
         /// <param name="count">The number of samples to process.</param>
         /// <param name="stride">The length of the processing step. Can be used to bypass samples.</param>
-        public unsafe void FilterOnce(double* input, double* output, int count, int stride)
-        {
-            fixed (Biquad* coeffs = _coeffs)
-            {
-                fixed (double* yn = _yn, yn1 = _yn1, yn2 = _yn2)
-                {
-                    for (int n = 0; n < count; n++)
-                    {
-                        var xn = *input * Gain;
-
-                        yn[0] = coeffs[0].B0 * xn + coeffs[0].B1 * _xn1 + coeffs[0].B2 * _xn2
-                                + coeffs[0].A1 * yn1[0] + coeffs[0].A2 * yn2[0];
-
-                        for (int i = 1; i < Count; i++)
-                            yn[i] = coeffs[i].B0 * yn[i - 1] + coeffs[i].B1 * yn1[i - 1] + coeffs[i].B2 * yn2[i - 1]
-                                    + coeffs[i].A1 * yn1[i] + coeffs[i].A2 * yn2[i];
-
-                        // Shift delay line elements.
-                        for (int i = 0; i < Count; i++)
-                        {
-                            yn2[i] = yn1[i];
-                            yn1[i] = yn[i];
-                        }
-                        _xn2 = _xn1;
-                        _xn1 = xn;
-
-                        // Store result and stride
-                        *output = yn[Count - 1];
-
-                        input += stride;
-                        output += stride;
-                    }
-                }
-            }
-        }
+        public abstract unsafe void FilterOnce(double* input, double* output, int count, int stride);
 
         /// <summary>
         /// Executes two sweeps of the filter (forward and backward). This is a zero-phase filter.
@@ -350,7 +261,7 @@ namespace Neuronic.Filters
         /// <filterpriority>1</filterpriority>
         public IEnumerator<Biquad> GetEnumerator()
         {
-            return _coeffs.Cast<Biquad>().GetEnumerator();
+            return Stages.Cast<Biquad>().GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -360,11 +271,159 @@ namespace Neuronic.Filters
 
         /// <summary>Gets the number of elements in the collection.</summary>
         /// <returns>The number of elements in the collection. </returns>
-        public int Count => _coeffs.Length;
+        public int Count => Stages.Length;
+
+        protected Biquad[] Stages => _coeffs;
 
         /// <summary>Gets the element at the specified index in the read-only list.</summary>
         /// <returns>The element at the specified index in the read-only list.</returns>
         /// <param name="index">The zero-based index of the element to get. </param>
-        public Biquad this[int index] => _coeffs[index];
+        public Biquad this[int index] => Stages[index];
+    }
+
+    public class DirectFormIBiquadChain : BiquadChain
+    {
+        private readonly State[] _states;
+        private double _ac = 1e-8;
+
+        public DirectFormIBiquadChain(IList<Biquad> coefficients) : base(coefficients)
+        {
+            _states = new State[Count];
+        }
+
+        public override void Reset()
+        {
+            Array.Clear(_states, 0, Count);
+        }
+
+        private unsafe double FilterSample(double input, State* state, Biquad* stage)
+        {
+            var result = input;
+            var ac = (_ac *= -1);
+            result = state[0].Process(result, stage[0], ac);
+            for (int i = 1; i < Count; i++)
+                result = state[i].Process(result, stage[i]);
+            return result;
+        }
+
+        public override unsafe void FilterOnce(float* input, float* output, int count, int stride)
+        {
+            fixed (Biquad* stage = Stages)
+            fixed (State* state = _states)
+            {
+                for (int n = 0; n < count; n++)
+                {
+                    *output = (float) FilterSample(*input, state, stage);
+
+                    input += stride;
+                    output += stride;
+                }
+            }
+        }
+
+        public override unsafe void FilterOnce(double* input, double* output, int count, int stride)
+        {
+            fixed (Biquad* stage = Stages)
+            fixed (State* state = _states)
+            {
+                for (int n = 0; n < count; n++)
+                {
+                    *output = FilterSample(*input, state, stage);
+
+                    input += stride;
+                    output += stride;
+                }
+            }
+        }
+
+        struct State
+        {
+            private double _x1, _x2, _y1, _y2;
+
+            public double Process(double x, Biquad s, double epsilon = 0d)
+            {
+                var output = s.B0 * x
+                    + s.B1 * _x1 + s.B2 * _x2
+                    - s.A1 * _y1 - s.A2 * _y2 + epsilon;
+                _x2 = _x1;
+                _y2 = _y1;
+                _x1 = x;
+                _y1 = output;
+
+                return output;
+            }
+        }
+    }
+
+    public class DirectFormIIBiquadChain : BiquadChain
+    {
+        private readonly State[] _states;
+        private double _ac = 1e-8;
+
+        public DirectFormIIBiquadChain(IList<Biquad> coefficients) : base(coefficients)
+        {
+            _states = new State[Count];
+        }
+
+        public override void Reset()
+        {
+            Array.Clear(_states, 0, Count);
+        }
+
+        private unsafe double FilterSample(double input, State* state, Biquad* stage)
+        {
+            var result = input;
+            var ac = (_ac *= -1);
+            result = state[0].Process(result, stage[0], ac);
+            for (int i = 1; i < Count; i++)
+                result = state[i].Process(result, stage[i]);
+            return result;
+        }
+
+        public override unsafe void FilterOnce(float* input, float* output, int count, int stride)
+        {
+            fixed (Biquad* stage = Stages)
+            fixed (State* state = _states)
+            {
+                for (int n = 0; n < count; n++)
+                {
+                    *output = (float)FilterSample(*input, state, stage);
+
+                    input += stride;
+                    output += stride;
+                }
+            }
+        }
+
+        public override unsafe void FilterOnce(double* input, double* output, int count, int stride)
+        {
+            fixed (Biquad* stage = Stages)
+            fixed (State* state = _states)
+            {
+                for (int n = 0; n < count; n++)
+                {
+                    *output = FilterSample(*input, state, stage);
+
+                    input += stride;
+                    output += stride;
+                }
+            }
+        }
+
+        struct State
+        {
+            private double _v1, _v2;
+
+            public double Process(double x, Biquad s, double epsilon = 0d)
+            {
+                var w = x - s.A1 * _v1 - s.A2 * _v2 + epsilon;
+                var output = s.B0 * w + s.B1 * _v1 + s.B2 * _v2;
+
+                _v2 = _v1;
+                _v1 = w;
+
+                return output;
+            }
+        }
     }
 }
