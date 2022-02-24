51f3892d0bba624e522d3c8afb68418e5db8a7e9
Alexei Labrada <labrada.alexei@gmail.com>
2022-02-24 18:35:57 -0500

    Implement unit tests for the chebyshev filters.

---
+++ Neuronic.Filters.Testing/ChebyshevIIHighPassTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neuronic.Filters.Chebyshev;
+using System.Linq;
+
+namespace Neuronic.Filters.Testing
+{
+    [TestClass]
+    public class ChebyshevIIHighPassTest
+    {
+        [TestMethod]
+        public void TestHighPass08()
+        {
+            const int order = 8;
+            const double fs = 44100d;
+            const double cutoffFrequency = 500d;
+            const double error = 1e-4;
+
+            var coeff = new HighPassChebyshevIICoefficients(order, fs, cutoffFrequency, 48);
+            var chain = coeff.Calculate();
+
+            var expected = Helpers.LoadCsv(
+                @"0.854998, -1.709830, 0.854998, 1.000000, -1.883222, 0.886929
+1.000000, -1.998433, 1.000000, 1.000000, -1.898298, 0.903343
+1.000000, -1.996492, 1.000000, 1.000000, -1.927393, 0.934394
+1.000000, -1.995120, 1.000000, 1.000000, -1.967963, 0.976469").ToList();
+
+            Assert.AreEqual(expected.Count, chain.Count);
+            for (int i = 0; i < expected.Count; i++)
+                Helpers.ValidateBiquad(expected[i], chain[i], error);
+        }
+
+        [TestMethod]
+        public void TestHighPass12()
+        {
+            const int order = 12;
+            const double fs = 32000d;
+            const double cutoffFrequency = 10d;
+            const double error = 1e-4;
+
+            var coeff = new HighPassChebyshevIICoefficients(order, fs, cutoffFrequency, 48);
+            var chain = coeff.Calculate();
+
+            var expected = Helpers.LoadCsv(
+                @"0.995933, -1.991866, 0.995933, 1.000000, -1.997892, 0.997893
+1.000000, -1.999999, 1.000000, 1.000000, -1.998035, 0.998036
+1.000000, -1.999999, 1.000000, 1.000000, -1.998311, 0.998313
+1.000000, -1.999998, 1.000000, 1.000000, -1.998702, 0.998706
+1.000000, -1.999997, 1.000000, 1.000000, -1.999182, 0.999186
+1.000000, -1.999996, 1.000000, 1.000000, -1.999717, 0.999722").ToList();
+
+            Assert.AreEqual(expected.Count, chain.Count);
+            for (int i = 0; i < expected.Count; i++)
+                Helpers.ValidateBiquad(expected[i], chain[i], error);
+        }
+
+        [TestMethod]
+        public void TestHighPass16()
+        {
+            const int order = 16;
+            const double fs = 31250d;
+            const double cutoffFrequency = 100d;
+            const double error = 1e-4;
+
+            var coeff = new HighPassChebyshevIICoefficients(order, fs, cutoffFrequency, 48);
+            var chain = coeff.Calculate();
+
+            var expected = Helpers.LoadCsv(
+                @"0.959945, -1.919886, 0.959945, 1.000000, -1.984108, 0.984176
+1.000000, -1.999966, 1.000000, 1.000000, -1.984682, 0.984779
+1.000000, -1.999910, 1.000000, 1.000000, -1.985811, 0.985964
+1.000000, -1.999837, 1.000000, 1.000000, -1.987462, 0.987687
+1.000000, -1.999758, 1.000000, 1.000000, -1.989580, 0.989884
+1.000000, -1.999686, 1.000000, 1.000000, -1.992096, 0.992474
+1.000000, -1.999630, 1.000000, 1.000000, -1.994925, 0.995359
+1.000000, -1.999600, 1.000000, 1.000000, -1.997966, 0.998430").ToList();
+
+            Assert.AreEqual(expected.Count, chain.Count);
+            for (int i = 0; i < expected.Count; i++)
+                Helpers.ValidateBiquad(expected[i], chain[i], error);
+        }
+    }
+}
--- Neuronic.Filters.Testing/ChebyshevIILowPassTest.cs
+++ Neuronic.Filters.Testing/ChebyshevIILowPassTest.cs
@@ -1,30 +1,83 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neuronic.Filters.Chebyshev;
+using System.Linq;
 
 namespace Neuronic.Filters.Testing
 {
     [TestClass]
     public class ChebyshevIILowPassTest
     {
-
         [TestMethod]
-        public void TestLowPass2()
+        public void TestLowPass08()
         {
-            const int order = 2;
+            const int order = 8;
             const double fs = 44100d;
-            const double cutoffFrequency = 2000d;
+            const double cutoffFrequency = 500d;
             const double error = 1e-4;
 
             var coeff = new LowPassChebyshevIICoefficients(order, fs, cutoffFrequency, 48);
             var chain = coeff.Calculate();
 
-            var expected = new[]
-            {
-                new Biquad(1, -1.8418888082730389, 1, 1, -1.9638716093794057, 0.96451523313991561),
-            };
+            var expected = Helpers.LoadCsv(
+                @"0.003542, -0.006627, 0.003542, 1.000000, -1.850372, 0.856450
+1.000000, -1.983612, 1.000000, 1.000000, -1.902676, 0.907306
+1.000000, -1.992667, 1.000000, 1.000000, -1.950079, 0.953551
+1.000000, -1.994727, 1.000000, 1.000000, -1.983090, 0.986058
+").ToList();
 
-            Assert.AreEqual(expected.Length, chain.Count);
-            for (int i = 0; i < expected.Length; i++)
+            Assert.AreEqual(expected.Count, chain.Count);
+            for (int i = 0; i < expected.Count; i++)
+                Helpers.ValidateBiquad(expected[i], chain[i], error);
+        }
+
+        [TestMethod]
+        public void TestLowPass12()
+        {
+            const int order = 12;
+            const double fs = 32000d;
+            const double cutoffFrequency = 2000d;
+            const double error = 1e-2;
+
+            var coeff = new LowPassChebyshevIICoefficients(order, fs, cutoffFrequency, 48);
+            var chain = coeff.Calculate();
+
+            var expected = Helpers.LoadCsv(
+                @"0.004224, 0.003363, 0.004224, 1.000000, -0.961310, 0.242000
+1.000000, -1.149174, 1.000000, 1.000000, -1.179991, 0.413183
+1.000000, -1.614136, 1.000000, 1.000000, -1.428074, 0.609010
+1.000000, -1.763423, 1.000000, 1.000000, -1.615363, 0.760065
+1.000000, -1.822796, 1.000000, 1.000000, -1.745316, 0.870017
+1.000000, -1.845222, 1.000000, 1.000000, -1.840403, 0.958141").ToList();
+
+            Assert.AreEqual(expected.Count, chain.Count);
+            for (int i = 0; i < expected.Count; i++)
+                Helpers.ValidateBiquad(expected[i], chain[i], error);
+        }
+
+        [TestMethod]
+        public void TestLowPass16()
+        {
+            const int order = 16;
+            const double fs = 31250d;
+            const double cutoffFrequency = 300d;
+            const double error = 1e-4;
+
+            var coeff = new LowPassChebyshevIICoefficients(order, fs, cutoffFrequency, 48);
+            var chain = coeff.Calculate();
+
+            var expected = Helpers.LoadCsv(
+                @"0.003088, -0.005107, 0.003088, 1.000000, -1.733557, 0.752389
+1.000000, -1.957258, 1.000000, 1.000000, -1.814016, 0.827649
+1.000000, -1.983684, 1.000000, 1.000000, -1.885766, 0.894795
+1.000000, -1.990975, 1.000000, 1.000000, -1.929719, 0.935987
+1.000000, -1.993917, 1.000000, 1.000000, -1.955793, 0.960505
+1.000000, -1.995325, 1.000000, 1.000000, -1.972269, 0.976106
+1.000000, -1.996028, 1.000000, 1.000000, -1.983739, 0.987102
+1.000000, -1.996328, 1.000000, 1.000000, -1.992751, 0.995910").ToList();
+            var expectedGain = 3.444463173412853e-25;
+
+            Assert.AreEqual(expected.Count, chain.Count);
+            for (int i = 0; i < expected.Count; i++)
                 Helpers.ValidateBiquad(expected[i], chain[i], error);
         }
     }
